2a4c923bf4ba7f4a70337db470e4fbde8deed231
Xiaochuan Ma <mxcins@gmail.com>
2024-08-23 20:16:17 +0800

    feat(ui): code

--- .changeset/changelog.js
+++ .changeset/changelog.js
@@ -7,6 +7,8 @@ const {
   getInfo,
   getInfoFromPullRequest,
 } = require("@changesets/get-github-info");
+const spawn = require("spawndamnit");
+const { getPackages } = require("@manypkg/get-packages");
 
 const changelogFunctions = {
   getDependencyReleaseLine: async (
@@ -95,37 +97,82 @@ const changelogFunctions = {
     console.log("commitFromSummary", commitFromSummary);
     console.log("usersFromSummary", usersFromSummary);
 
-    const links = await (async () => {
-      if (prFromSummary !== undefined) {
-        let { links } = await getInfoFromPullRequest({
-          repo: options.repo,
-          pull: prFromSummary,
-        });
-        if (commitFromSummary) {
-          const shortCommitId = commitFromSummary.slice(0, 7);
-          links = {
-            ...links,
-            commit: `[\`${shortCommitId}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
-          };
-        }
-        return links;
+    // const links = await (async () => {
+    //   if (prFromSummary !== undefined) {
+    //     let { links } = await getInfoFromPullRequest({
+    //       repo: options.repo,
+    //       pull: prFromSummary,
+    //     });
+    //     if (commitFromSummary) {
+    //       const shortCommitId = commitFromSummary.slice(0, 7);
+    //       links = {
+    //         ...links,
+    //         commit: `[\`${shortCommitId}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
+    //       };
+    //     }
+    //     return links;
+    //   }
+    //   const commitToFetchFrom = commitFromSummary || changeset.commit;
+    //   console.log("commitToFetchFrom", commitToFetchFrom);
+    //   if (commitToFetchFrom) {
+    //     let info = await getInfo({
+    //       repo: options.repo,
+    //       commit: commitToFetchFrom,
+    //     });
+    //     const { links } = info;
+    //     console.log(
+    //       "commitToFetchFrom, links\n\n\n",
+    //       JSON.stringify(info, undefined, 2)
+    //     );
+    //     return links;
+    //   }
+    //   return {
+    //     commit: null,
+    //     pull: null,
+    //     user: null,
+    //   };
+    // })();
+    const commit = changeset.commit;
+    const { stdout } = await spawn(
+      "git",
+      ["log", '--pretty=format:"%H - %s"'],
+      {
+        cwd: process.cwd(),
       }
-      const commitToFetchFrom = commitFromSummary || changeset.commit;
-      console.log("commitToFetchFrom", commitToFetchFrom);
-      if (commitToFetchFrom) {
-        let { links } = await getInfo({
-          repo: options.repo,
-          commit: commitToFetchFrom,
-        });
-        console.log("commitToFetchFrom, links", links);
-        return links;
-      }
-      return {
-        commit: null,
-        pull: null,
-        user: null,
-      };
-    })();
+    );
+
+    const commits = stdout.split("\n").map((row) => {
+      const match = row.match(/([a-f0-9]{40}) - (.*)/);
+      return { commit: match[1], message: match[2] };
+    });
+
+    const packages = await getPackages(process.cwd());
+
+    console.log("packages", packages);
+
+    changesets.releases.map((release) => {
+      const name = release.name;
+    });
+
+    // let findCommit = false;
+
+    // const before = [];
+
+    // commits.forEach((row) => {
+    //   if (row.commit === commit) {
+    //     findCommit = true;
+    //     console.log("找到了 commit", row.commit, row.message);
+    //     return;
+    //   }
+    //   if (findCommit && row.message.startsWith("feat(ui):")) {
+    //     before.push(row);
+    //   }
+    //   if (row.message.startsWith("chore: changeset")) {
+    //     findCommit = false;
+    //   }
+    // });
+
+    // console.log("before\n\n", JSON.stringify(before, undefined, 2));
 
     const users = usersFromSummary.length
       ? usersFromSummary
